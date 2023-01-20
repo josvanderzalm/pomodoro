ec7f9fcef05efd2d76a06f7fd90891310d627cd6
Jos van der Zalm <jos.v.d.zalm@gmail.com>
2023-01-20 18:57:39 +0100

    wip

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    count = 0
+    for (let index = 0; index < recover; index++) {
+        count += 1
+        basic.showNumber(count)
+        basic.pause(200)
+    }
+    basic.showIcon(IconNames.Heart)
 })
+let count = 0
+let work = 25
+let recover = 5
+let action = "work"
+
